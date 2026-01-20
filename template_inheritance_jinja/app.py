81264182ae1f387fd062591d40ec96f0bd7904a1
Amisha103 <bhatiaamisha74@gmail.com>
2026-01-20 20:39:30 +0530

    rendering templates

---
+++ template_inheritance_jinja/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+@app.route("/submit", methods=["POST"])
+def submit():
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if username == "amisha" and password == "pass":
+        return render_template("welcome.html", name = username)
+    else:
+        return "Invalid credentials"
