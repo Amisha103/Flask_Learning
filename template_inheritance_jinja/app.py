4c90ba0a423bfc1cc086c362ca10bb25f2876c19
Amisha103 <bhatiaamisha74@gmail.com>
2026-01-21 20:17:43 +0530

    learned template inheritance

--- template_inheritance_jinja/app.py
+++ template_inheritance_jinja/app.py
@@ -3,5 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def student_profile():
-    return render_template("profile.html", name = "amisha", is_topper = True, subjects = ["DSA", "DBMS", "Networking"])
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+# def student_profile():
+#     return render_template("profile.html", name = "amisha", is_topper = True, subjects = ["DSA", "DBMS", "Networking"])
+
